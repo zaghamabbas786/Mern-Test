9128923cfffbcab97ad9f2077b01634a12ba855f
zagham <zaghama96@gmail.com>
2025-11-14 00:28:29 +0500

    ADDED DETAILS

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,8 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
 exports.connectDB = (mongoose) => {
   try {
-    mongoose.connect("mongodb://localhost:27017/ecommerce", {
+    mongoose.connect(process.env.DATABASE || "mongodb://localhost:27017/ecommerce", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
